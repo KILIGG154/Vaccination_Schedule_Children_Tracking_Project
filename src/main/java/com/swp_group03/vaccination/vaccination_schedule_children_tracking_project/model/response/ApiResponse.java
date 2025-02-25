996c80ea6367c3a0acf1f1d5920fc53340652d05
YukiiMin <hoangtuanminh1104@gmail.com>
2025-02-25 12:38:02 +0700

    Login-Token

--- src/main/java/com/swp_group03/vaccination/vaccination_schedule_children_tracking_project/model/response/ApiResponse.java
+++ src/main/java/com/swp_group03/vaccination/vaccination_schedule_children_tracking_project/model/response/ApiResponse.java
@@ -7,6 +7,7 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@Builder
 public class ApiResponse<T> {
     private int code;
     private String message;
--- src/main/java/com/swp_group03/vaccination/vaccination_schedule_children_tracking_project/service/AuthenticationService.java
+++ src/main/java/com/swp_group03/vaccination/vaccination_schedule_children_tracking_project/service/AuthenticationService.java
@@ -1,26 +1,82 @@
 package com.swp_group03.vaccination.vaccination_schedule_children_tracking_project.service;
 
+import com.nimbusds.jose.*;
+import com.nimbusds.jose.crypto.MACSigner;
+import com.nimbusds.jwt.JWTClaimsSet;
+//import com.swp_group03.vaccination.vaccination_schedule_children_tracking_project.config.JwtConfig;
 import com.swp_group03.vaccination.vaccination_schedule_children_tracking_project.controller.AuthenticationController;
 import com.swp_group03.vaccination.vaccination_schedule_children_tracking_project.exception.AppException;
 import com.swp_group03.vaccination.vaccination_schedule_children_tracking_project.exception.ErrorCode;
 import com.swp_group03.vaccination.vaccination_schedule_children_tracking_project.model.request.account.AuthenticationRequest;
+import com.swp_group03.vaccination.vaccination_schedule_children_tracking_project.model.response.AuthenticationResponse;
 import com.swp_group03.vaccination.vaccination_schedule_children_tracking_project.repository.UserRepo;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import lombok.experimental.NonFinal;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+
 @Service
 @RequiredArgsConstructor
 @FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
+@Slf4j
 public class AuthenticationService {
     UserRepo userRepo;
 
-    public boolean authenticate(AuthenticationRequest request) {
-        var account = userRepo.findByUsername(request.getUsername()).orElseThrow(() -> new AppException(ErrorCode.USER_NOT_EXIST));
+    @NonFinal
+    @Value("${jwt.secretKey}")
+    protected String SIGNER_KEY;
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
         PasswordEncoder passwordEncoder = new BCryptPasswordEncoder(10);
-        return passwordEncoder.matches(request.getPassword(), account.getPassword());
+
+        var account = userRepo.findByUsername(request.getUsername()).orElseThrow(() -> new AppException(ErrorCode.USERNAME_NOT_EXIT));
+
+         boolean authenticated = passwordEncoder.matches(request.getPassword(), account.getPassword());
+
+    if(!authenticated) {
+        throw new AppException(ErrorCode.UNAUTHENTICATED);
     }
+
+        var token = generateToken(account.getUsername());
+
+        return AuthenticationResponse.builder()
+                .token(token).authenticated(authenticated)
+                .build();
+    }
+
+
+    private String generateToken(String username){
+        JWSHeader header = new JWSHeader(JWSAlgorithm.HS512);
+
+        //Data trong body (Payload của JWT gọi là Claim, tập hợp 1 chuỗi thành 1 Set)
+        JWTClaimsSet claimsSet = new JWTClaimsSet.Builder()
+                .subject(username)
+                .issuer("swp_group3.com")
+                .issueTime(new Date())
+                .expirationTime(new Date(Instant.now().plus(1, ChronoUnit.HOURS).toEpochMilli()))
+                .claim("customeClain", "Custom")
+                .build();
+
+        Payload payload = new Payload(claimsSet.toJSONObject());
+
+        JWSObject jwsObject = new JWSObject(header,payload);
+
+        try {
+            jwsObject.sign(new MACSigner(SIGNER_KEY.getBytes()));
+            return jwsObject.serialize();
+        } catch (JOSEException e) {
+            log.error("Cannot create Token", e);
+            throw new RuntimeException(e);
+        }
+    }
+
 }
