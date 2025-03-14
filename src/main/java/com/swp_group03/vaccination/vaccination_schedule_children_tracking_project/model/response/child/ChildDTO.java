8d1c000225ae74cf91f1854f94737594fc113832
KILIGG154 <chutanphat56@gmail.com>
2025-03-14 16:30:57 +0700

    update childdto

--- src/main/java/com/swp_group03/vaccination/vaccination_schedule_children_tracking_project/model/response/child/ChildDTO.java
+++ src/main/java/com/swp_group03/vaccination/vaccination_schedule_children_tracking_project/model/response/child/ChildDTO.java
@@ -13,18 +13,18 @@ public class ChildDTO {
     private int id;
     private String name;
     private Date dob;
-//    private String height;
-//    private String weight;
-//    private Gender gender;
+    private String height;
+    private String weight;
+    private Gender gender;
 //    private String urlImage;
 
     public ChildDTO(Child child) {
         this.id = child.getChild_id();
         this.name = child.getName();
         this.dob = child.getDob();
-//        this.height = child.getHeight();
-//        this.weight = child.getWeight();
-//        this.gender = child.getGender();
+        this.height = child.getHeight();
+        this.weight = child.getWeight();
+        this.gender = child.getGender();
 //        this.urlImage = child.getUrlImage();
     }
 }
